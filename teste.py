771cd15c929c9830fa2bb485e02f39748cdd8c49
BrenoLRocha <blrocha@stefanini.com>
2022-03-17 16:59:35 -0300

    deve fucionar

---
+++ teste.py
@@ -0,0 +1,8 @@
+import rocketpy 
+
+Env = Environment(railLength= 6,
+                  gravity= 9.8,
+                  latitude=32.94258333,
+                  longitude=-106.91488889,
+                  elevation=1189
+                  )
